996dd3b1338a3fdaaa27d63da85777637eadb77a
Treetrain1 <treetrain1@gmail.com>
2023-08-07 04:35:18 -0500

    initial 1.20.2

--- src/main/java/de/siphalor/nbtcrafting/mixin/network/MixinServerPlayNetworkHandler.java
+++ src/main/java/de/siphalor/nbtcrafting/mixin/network/MixinServerPlayNetworkHandler.java
@@ -3,17 +3,19 @@ package de.siphalor.nbtcrafting.mixin.network;
 import de.siphalor.nbtcrafting.network.ServerNetworkHandlerAccess;
 
 import net.minecraft.network.ClientConnection;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerCommonNetworkHandler;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ServerPlayNetworkHandler.class)
-public class MixinServerPlayNetworkHandler implements ServerNetworkHandlerAccess {
+public abstract class MixinServerPlayNetworkHandler extends ServerCommonNetworkHandler implements ServerNetworkHandlerAccess {
 
-	@Shadow
-	@Final
-	private ClientConnection connection;
+	private MixinServerPlayNetworkHandler(MinecraftServer server, ClientConnection connection, int keepAliveId) {
+		super(server, connection, keepAliveId);
+	}
 
 	@Override
 	public ClientConnection nbtCrafting$getConnection() {
